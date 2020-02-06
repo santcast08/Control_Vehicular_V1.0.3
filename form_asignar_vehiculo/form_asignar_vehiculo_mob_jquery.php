621f63c04148f9035a6df207c702729190130dcc
santcast08 <59621824+santcast08@users.noreply.github.com>
2020-02-06 17:43:30 -0500

    Add files via upload

---
+++ form_asignar_vehiculo/form_asignar_vehiculo_mob_jquery.php
@@ -0,0 +1,325 @@
+
+function scJQGeneralAdd() {
+  scLoadScInput('input:text.sc-js-input');
+  scLoadScInput('input:password.sc-js-input');
+  scLoadScInput('input:checkbox.sc-js-input');
+  scLoadScInput('input:radio.sc-js-input');
+  scLoadScInput('select.sc-js-input');
+  scLoadScInput('textarea.sc-js-input');
+
+} // scJQGeneralAdd
+
+function scFocusField(sField) {
+  var $oField = $('#id_sc_field_' + sField);
+
+  if (0 == $oField.length) {
+    $oField = $('input[name=' + sField + ']');
+  }
+
+  if (0 == $oField.length && document.F1.elements[sField]) {
+    $oField = $(document.F1.elements[sField]);
+  }
+
+  if (false == scSetFocusOnField($oField) && $("#id_ac_" + sField).length > 0) {
+    if (false == scSetFocusOnField($("#id_ac_" + sField))) {
+      setTimeout(function() { scSetFocusOnField($("#id_ac_" + sField)); }, 500);
+    }
+  }
+  else {
+    setTimeout(function() { scSetFocusOnField($oField); }, 500);
+  }
+} // scFocusField
+
+function scSetFocusOnField($oField) {
+  if ($oField.length > 0 && $oField[0].offsetHeight > 0 && $oField[0].offsetWidth > 0 && !$oField[0].disabled) {
+    $oField[0].focus();
+    return true;
+  }
+  return false;
+} // scSetFocusOnField
+
+function scEventControl_init(iSeqRow) {
+  scEventControl_data["asignar_vehiculo_fecha" + iSeqRow] = {"blur": false, "change": false, "autocomp": false, "original": "", "calculated": ""};
+  scEventControl_data["usuario_idusuario" + iSeqRow] = {"blur": false, "change": false, "autocomp": false, "original": "", "calculated": ""};
+  scEventControl_data["idasignar_vehiculo" + iSeqRow] = {"blur": false, "change": false, "autocomp": false, "original": "", "calculated": ""};
+}
+
+function scEventControl_active(iSeqRow) {
+  if (scEventControl_data["asignar_vehiculo_fecha" + iSeqRow]["blur"]) {
+    return true;
+  }
+  if (scEventControl_data["asignar_vehiculo_fecha" + iSeqRow]["change"]) {
+    return true;
+  }
+  if (scEventControl_data["usuario_idusuario" + iSeqRow]["blur"]) {
+    return true;
+  }
+  if (scEventControl_data["usuario_idusuario" + iSeqRow]["change"]) {
+    return true;
+  }
+  if (scEventControl_data["idasignar_vehiculo" + iSeqRow]["blur"]) {
+    return true;
+  }
+  if (scEventControl_data["idasignar_vehiculo" + iSeqRow]["change"]) {
+    return true;
+  }
+  return false;
+} // scEventControl_active
+
+function scEventControl_onFocus(oField, iSeq) {
+  var fieldId, fieldName;
+  fieldId = $(oField).attr("id");
+  fieldName = fieldId.substr(12);
+  scEventControl_data[fieldName]["blur"] = true;
+  if ("usuario_idusuario" + iSeq == fieldName) {
+    scEventControl_data[fieldName]["blur"] = false;
+  }
+  if ("idasignar_vehiculo" + iSeq == fieldName) {
+    scEventControl_data[fieldName]["blur"] = false;
+  }
+  scEventControl_data[fieldName]["change"] = false;
+} // scEventControl_onFocus
+
+function scEventControl_onBlur(sFieldName) {
+  scEventControl_data[sFieldName]["blur"] = false;
+  if (scEventControl_data[sFieldName]["change"]) {
+        if (scEventControl_data[sFieldName]["original"] == $("#id_sc_field_" + sFieldName).val() || scEventControl_data[sFieldName]["calculated"] == $("#id_sc_field_" + sFieldName).val()) {
+          scEventControl_data[sFieldName]["change"] = false;
+        }
+  }
+} // scEventControl_onBlur
+
+function scEventControl_onChange(sFieldName) {
+  scEventControl_data[sFieldName]["change"] = false;
+} // scEventControl_onChange
+
+function scEventControl_onAutocomp(sFieldName) {
+  scEventControl_data[sFieldName]["autocomp"] = false;
+} // scEventControl_onChange
+
+var scEventControl_data = {};
+
+function scJQEventsAdd(iSeqRow) {
+  $('#id_sc_field_idasignar_vehiculo' + iSeqRow).bind('blur', function() { sc_form_asignar_vehiculo_idasignar_vehiculo_onblur(this, iSeqRow) })
+                                                .bind('focus', function() { sc_form_asignar_vehiculo_idasignar_vehiculo_onfocus(this, iSeqRow) });
+  $('#id_sc_field_asignar_vehiculo_fecha' + iSeqRow).bind('blur', function() { sc_form_asignar_vehiculo_asignar_vehiculo_fecha_onblur(this, iSeqRow) })
+                                                    .bind('focus', function() { sc_form_asignar_vehiculo_asignar_vehiculo_fecha_onfocus(this, iSeqRow) });
+  $('#id_sc_field_usuario_idusuario' + iSeqRow).bind('blur', function() { sc_form_asignar_vehiculo_usuario_idusuario_onblur(this, iSeqRow) })
+                                               .bind('focus', function() { sc_form_asignar_vehiculo_usuario_idusuario_onfocus(this, iSeqRow) });
+} // scJQEventsAdd
+
+function sc_form_asignar_vehiculo_idasignar_vehiculo_onblur(oThis, iSeqRow) {
+  do_ajax_form_asignar_vehiculo_mob_validate_idasignar_vehiculo();
+  scCssBlur(oThis);
+}
+
+function sc_form_asignar_vehiculo_idasignar_vehiculo_onfocus(oThis, iSeqRow) {
+  scEventControl_onFocus(oThis, iSeqRow);
+  scCssFocus(oThis);
+}
+
+function sc_form_asignar_vehiculo_asignar_vehiculo_fecha_onblur(oThis, iSeqRow) {
+  do_ajax_form_asignar_vehiculo_mob_validate_asignar_vehiculo_fecha();
+  scCssBlur(oThis);
+}
+
+function sc_form_asignar_vehiculo_asignar_vehiculo_fecha_onfocus(oThis, iSeqRow) {
+  scEventControl_onFocus(oThis, iSeqRow);
+  scCssFocus(oThis);
+}
+
+function sc_form_asignar_vehiculo_usuario_idusuario_onblur(oThis, iSeqRow) {
+  do_ajax_form_asignar_vehiculo_mob_validate_usuario_idusuario();
+  scCssBlur(oThis);
+}
+
+function sc_form_asignar_vehiculo_usuario_idusuario_onfocus(oThis, iSeqRow) {
+  scEventControl_onFocus(oThis, iSeqRow);
+  scCssFocus(oThis);
+}
+
+function displayChange_block(block, status) {
+	if ("0" == block) {
+		displayChange_block_0(status);
+	}
+}
+
+function displayChange_block_0(status) {
+	displayChange_field("Asignar_Vehiculo_Fecha", "", status);
+	displayChange_field("usuario_idusuario", "", status);
+	displayChange_field("idAsignar_Vehiculo", "", status);
+}
+
+function displayChange_row(row, status) {
+	displayChange_field_asignar_vehiculo_fecha(row, status);
+	displayChange_field_usuario_idusuario(row, status);
+	displayChange_field_idasignar_vehiculo(row, status);
+}
+
+function displayChange_field(field, row, status) {
+	if ("asignar_vehiculo_fecha" == field) {
+		displayChange_field_asignar_vehiculo_fecha(row, status);
+	}
+	if ("usuario_idusuario" == field) {
+		displayChange_field_usuario_idusuario(row, status);
+	}
+	if ("idasignar_vehiculo" == field) {
+		displayChange_field_idasignar_vehiculo(row, status);
+	}
+}
+
+function displayChange_field_asignar_vehiculo_fecha(row, status) {
+}
+
+function displayChange_field_usuario_idusuario(row, status) {
+	if ("on" == status) {
+		$("#id_sc_field_usuario_idusuario" + row).select2("destroy");
+		scJQSelect2Add(row, "usuario_idusuario");
+	}
+}
+
+function displayChange_field_idasignar_vehiculo(row, status) {
+	if ("on" == status) {
+		$("#id_sc_field_idasignar_vehiculo" + row).select2("destroy");
+		scJQSelect2Add(row, "idasignar_vehiculo");
+	}
+}
+
+function scResetPagesDisplay() {
+	$(".sc-form-page").show();
+}
+
+function scHidePage(pageNo) {
+	$("#id_form_asignar_vehiculo_mob_form" + pageNo).hide();
+}
+
+function scCheckNoPageSelected() {
+	if (!$(".sc-form-page").filter(".scTabActive").filter(":visible").length) {
+		var inactiveTabs = $(".sc-form-page").filter(".scTabInactive").filter(":visible");
+		if (inactiveTabs.length) {
+			var tabNo = $(inactiveTabs[0]).attr("id").substr(33);
+		}
+	}
+}
+var sc_jq_calendar_value = {};
+
+function scJQCalendarAdd(iSeqRow) {
+  $("#id_sc_field_asignar_vehiculo_fecha" + iSeqRow).datepicker({
+    beforeShow: function(input, inst) {
+      var $oField = $(this),
+          aParts  = $oField.val().split(" "),
+          sTime   = "";
+      sc_jq_calendar_value["#id_sc_field_asignar_vehiculo_fecha" + iSeqRow] = $oField.val();
+    },
+    onClose: function(dateText, inst) {
+      setTimeout(function() { do_ajax_form_asignar_vehiculo_mob_validate_asignar_vehiculo_fecha(iSeqRow); }, 200);
+    },
+    showWeek: true,
+    numberOfMonths: 1,
+    changeMonth: true,
+    changeYear: true,
+    yearRange: 'c-5:c+5',
+    dayNames: ["<?php        echo html_entity_decode($this->Ini->Nm_lang['lang_days_sund'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);        ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_days_mond'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);        ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_days_tued'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);        ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_days_wend'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);        ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_days_thud'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);        ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_days_frid'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);        ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_days_satd'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);        ?>"],
+    dayNamesMin: ["<?php     echo html_entity_decode($this->Ini->Nm_lang['lang_substr_days_sund'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_substr_days_mond'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_substr_days_tued'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_substr_days_wend'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_substr_days_thud'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_substr_days_frid'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_substr_days_satd'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>"],
+    monthNames: ["<?php      echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_janu"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_febr"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_marc"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_apri"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_mayy"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_june"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_july"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_augu"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_sept"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_octo"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_nove"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>","<?php echo html_entity_decode($this->Ini->Nm_lang["lang_mnth_dece"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);      ?>"],
+    monthNamesShort: ["<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_janu'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);   ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_febr'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);   ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_marc'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);   ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_apri'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);   ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_mayy'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);   ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_june'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);   ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_july'], ENT_COMPAT, $_SESSION['scriptcase']['charset']);   ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_augu'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_sept'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_octo'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_nove'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>","<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_mnth_dece'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>"],
+    weekHeader: "<?php echo html_entity_decode($this->Ini->Nm_lang['lang_shrt_days_sem'], ENT_COMPAT, $_SESSION['scriptcase']['charset']); ?>",
+    firstDay: <?php echo $this->jqueryCalendarWeekInit("" . $_SESSION['scriptcase']['reg_conf']['date_week_ini'] . ""); ?>,
+    dateFormat: "<?php echo $this->jqueryCalendarDtFormat("" . str_replace(array('/', 'aaaa', $_SESSION['scriptcase']['reg_conf']['date_sep']), array('', 'yyyy', ''), $this->field_config['asignar_vehiculo_fecha']['date_format']) . "", "" . $_SESSION['scriptcase']['reg_conf']['date_sep'] . ""); ?>",
+    showOtherMonths: true,
+    showOn: "button",
+    buttonImage: "<?php echo $this->jqueryIconFile('calendar'); ?>",
+    buttonImageOnly: true,
+    currentText: "<?php  echo html_entity_decode($this->Ini->Nm_lang["lang_per_today"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);       ?>",
+    closeText: "<?php  echo html_entity_decode($this->Ini->Nm_lang["lang_btns_mess_clse"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]);       ?>",
+  });
+
+} // scJQCalendarAdd
+
+function scJQUploadAdd(iSeqRow) {
+} // scJQUploadAdd
+
+function scJQSelect2Add(seqRow, specificField) {
+  if (null == specificField || "usuario_idusuario" == specificField) {
+    scJQSelect2Add_usuario_idusuario(seqRow);
+  }
+  if (null == specificField || "idasignar_vehiculo" == specificField) {
+    scJQSelect2Add_idasignar_vehiculo(seqRow);
+  }
+} // scJQSelect2Add
+
+function scJQSelect2Add_usuario_idusuario(seqRow) {
+  $("#id_sc_field_usuario_idusuario" + seqRow).select2(
+    {
+      language: {
+        noResults: function() {
+          return "<?php echo $this->Ini->Nm_lang['lang_autocomp_notfound'] ?>";
+        },
+        searching: function() {
+          return "<?php echo $this->Ini->Nm_lang['lang_autocomp_searching'] ?>";
+        }
+      }
+    }
+  );
+} // scJQSelect2Add
+
+function scJQSelect2Add_idasignar_vehiculo(seqRow) {
+  $("#id_sc_field_idasignar_vehiculo" + seqRow).select2(
+    {
+      language: {
+        noResults: function() {
+          return "<?php echo $this->Ini->Nm_lang['lang_autocomp_notfound'] ?>";
+        },
+        searching: function() {
+          return "<?php echo $this->Ini->Nm_lang['lang_autocomp_searching'] ?>";
+        }
+      }
+    }
+  );
+} // scJQSelect2Add
+
+
+function scJQElementsAdd(iLine) {
+  scJQEventsAdd(iLine);
+  scEventControl_init(iLine);
+  scJQCalendarAdd(iLine);
+  scJQUploadAdd(iLine);
+  scJQSelect2Add(iLine);
+} // scJQElementsAdd
+
+var scBtnGrpStatus = {};
+function scBtnGrpShow(sGroup) {
+  if (typeof(scBtnGrpShowMobile) === typeof(function(){})) { return scBtnGrpShowMobile(sGroup); };
+  var btnPos = $('#sc_btgp_btn_' + sGroup).offset();
+  scBtnGrpStatus[sGroup] = 'open';
+  $('#sc_btgp_btn_' + sGroup).mouseout(function() {
+    scBtnGrpStatus[sGroup] = '';
+    setTimeout(function() {
+      scBtnGrpHide(sGroup);
+    }, 1000);
+  }).mouseover(function() {
+    scBtnGrpStatus[sGroup] = 'over';
+  });
+  $('#sc_btgp_div_' + sGroup + ' span a').click(function() {
+    scBtnGrpStatus[sGroup] = 'out';
+    scBtnGrpHide(sGroup);
+  });
+  $('#sc_btgp_div_' + sGroup).css({
+    'left': btnPos.left
+  })
+  .mouseover(function() {
+    scBtnGrpStatus[sGroup] = 'over';
+  })
+  .mouseleave(function() {
+    scBtnGrpStatus[sGroup] = 'out';
+    setTimeout(function() {
+      scBtnGrpHide(sGroup);
+    }, 1000);
+  })
+  .show('fast');
+}
+function scBtnGrpHide(sGroup) {
+  if ('over' != scBtnGrpStatus[sGroup]) {
+    $('#sc_btgp_div_' + sGroup).hide('fast');
+  }
+}
