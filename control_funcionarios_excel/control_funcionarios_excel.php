092371c77a96615bf7faba6f8431c2d321f89d19
santcast08 <59621824+santcast08@users.noreply.github.com>
2020-02-06 17:38:16 -0500

    Add files via upload

---
+++ control_funcionarios_excel/control_funcionarios_excel.php
@@ -0,0 +1,3 @@
+<?php 
+  include_once('../control_funcionarios_excel/index.php'); 
+?> 
---
+++ control_rutas_excel/control_rutas_excel_jquery.php
@@ -0,0 +1,259 @@
+
+function scJQGeneralAdd() {
+  scLoadScInput('input:text.sc-js-input');
+  scLoadScInput('input:password.sc-js-input');
+  scLoadScInput('input:checkbox.sc-js-input');
+  scLoadScInput('input:radio.sc-js-input');
+  scLoadScInput('select.sc-js-input');
+  scLoadScInput('textarea.sc-js-input');
+
+} // scJQGeneralAdd
+
+function scFocusField(sField) {
+  var $oField = $('#id_sc_field_' + sField);
+
+  if (0 == $oField.length) {
+    $oField = $('input[name=' + sField + ']');
+  }
+
+  if (0 == $oField.length && document.F1.elements[sField]) {
+    $oField = $(document.F1.elements[sField]);
+  }
+
+  if (false == scSetFocusOnField($oField) && $("#id_ac_" + sField).length > 0) {
+    if (false == scSetFocusOnField($("#id_ac_" + sField))) {
+      setTimeout(function() { scSetFocusOnField($("#id_ac_" + sField)); }, 500);
+    }
+  }
+  else {
+    setTimeout(function() { scSetFocusOnField($oField); }, 500);
+  }
+} // scFocusField
+
+function scSetFocusOnField($oField) {
+  if ($oField.length > 0 && $oField[0].offsetHeight > 0 && $oField[0].offsetWidth > 0 && !$oField[0].disabled) {
+    $oField[0].focus();
+    return true;
+  }
+  return false;
+} // scSetFocusOnField
+
+function scEventControl_init(iSeqRow) {
+  scEventControl_data["archivo" + iSeqRow] = {"blur": false, "change": false, "autocomp": false, "original": "", "calculated": ""};
+  scEventControl_data["ubicacion" + iSeqRow] = {"blur": false, "change": false, "autocomp": false, "original": "", "calculated": ""};
+}
+
+function scEventControl_active(iSeqRow) {
+  if (scEventControl_data["ubicacion" + iSeqRow]["blur"]) {
+    return true;
+  }
+  if (scEventControl_data["ubicacion" + iSeqRow]["change"]) {
+    return true;
+  }
+  return false;
+} // scEventControl_active
+
+function scEventControl_onFocus(oField, iSeq) {
+  var fieldId, fieldName;
+  fieldId = $(oField).attr("id");
+  fieldName = fieldId.substr(12);
+  scEventControl_data[fieldName]["blur"] = true;
+  scEventControl_data[fieldName]["change"] = false;
+} // scEventControl_onFocus
+
+function scEventControl_onBlur(sFieldName) {
+  scEventControl_data[sFieldName]["blur"] = false;
+  if (scEventControl_data[sFieldName]["change"]) {
+        if (scEventControl_data[sFieldName]["original"] == $("#id_sc_field_" + sFieldName).val() || scEventControl_data[sFieldName]["calculated"] == $("#id_sc_field_" + sFieldName).val()) {
+          scEventControl_data[sFieldName]["change"] = false;
+        }
+  }
+} // scEventControl_onBlur
+
+function scEventControl_onChange(sFieldName) {
+  scEventControl_data[sFieldName]["change"] = false;
+} // scEventControl_onChange
+
+function scEventControl_onAutocomp(sFieldName) {
+  scEventControl_data[sFieldName]["autocomp"] = false;
+} // scEventControl_onChange
+
+var scEventControl_data = {};
+
+function scJQEventsAdd(iSeqRow) {
+  $('#id_sc_field_archivo' + iSeqRow).bind('blur', function() { sc_control_rutas_excel_archivo_onblur(this, iSeqRow) })
+                                     .bind('focus', function() { sc_control_rutas_excel_archivo_onfocus(this, iSeqRow) });
+  $('#id_sc_field_ubicacion' + iSeqRow).bind('blur', function() { sc_control_rutas_excel_ubicacion_onblur(this, iSeqRow) })
+                                       .bind('focus', function() { sc_control_rutas_excel_ubicacion_onfocus(this, iSeqRow) });
+} // scJQEventsAdd
+
+function sc_control_rutas_excel_archivo_onblur(oThis, iSeqRow) {
+  scCssBlur(oThis);
+}
+
+function sc_control_rutas_excel_archivo_onfocus(oThis, iSeqRow) {
+  scCssFocus(oThis);
+}
+
+function sc_control_rutas_excel_ubicacion_onblur(oThis, iSeqRow) {
+  do_ajax_control_rutas_excel_validate_ubicacion();
+  scCssBlur(oThis);
+}
+
+function sc_control_rutas_excel_ubicacion_onfocus(oThis, iSeqRow) {
+  scEventControl_onFocus(oThis, iSeqRow);
+  scCssFocus(oThis);
+}
+
+function displayChange_block(block, status) {
+	if ("0" == block) {
+		displayChange_block_0(status);
+	}
+}
+
+function displayChange_block_0(status) {
+	displayChange_field("archivo", "", status);
+	displayChange_field("ubicacion", "", status);
+}
+
+function displayChange_row(row, status) {
+	displayChange_field_archivo(row, status);
+	displayChange_field_ubicacion(row, status);
+}
+
+function displayChange_field(field, row, status) {
+	if ("archivo" == field) {
+		displayChange_field_archivo(row, status);
+	}
+	if ("ubicacion" == field) {
+		displayChange_field_ubicacion(row, status);
+	}
+}
+
+function displayChange_field_archivo(row, status) {
+}
+
+function displayChange_field_ubicacion(row, status) {
+}
+
+function scResetPagesDisplay() {
+	$(".sc-form-page").show();
+}
+
+function scHidePage(pageNo) {
+	$("#id_control_rutas_excel_form" + pageNo).hide();
+}
+
+function scCheckNoPageSelected() {
+	if (!$(".sc-form-page").filter(".scTabActive").filter(":visible").length) {
+		var inactiveTabs = $(".sc-form-page").filter(".scTabInactive").filter(":visible");
+		if (inactiveTabs.length) {
+			var tabNo = $(inactiveTabs[0]).attr("id").substr(27);
+		}
+	}
+}
+function scJQUploadAdd(iSeqRow) {
+  $("#id_sc_field_archivo" + iSeqRow).fileupload({
+    datatype: "json",
+    url: "control_rutas_excel_ul_save.php",
+    dropZone: $("#hidden_field_data_archivo" + iSeqRow),
+    formData: function() {
+      return [
+        {name: 'param_field', value: 'archivo'},
+        {name: 'param_seq', value: '<?php echo $this->Ini->sc_page; ?>'},
+        {name: 'upload_file_row', value: iSeqRow}
+      ];
+    },
+    progress: function(e, data) {
+      var loader, progress;
+      if (data.lengthComputable && window.FormData !== undefined) {
+        loader = $("#id_img_loader_archivo" + iSeqRow);
+        progress = parseInt(data.loaded / data.total * 100, 10);
+        loader.show().find("div").css("width", progress + "%");
+      }
+      else {
+        loader = $("#id_ajax_loader_archivo" + iSeqRow);
+        loader.show();
+      }
+    },
+    done: function(e, data) {
+      var fileData, respData, respPos, respMsg, thumbDisplay, checkDisplay, var_ajax_img_thumb, oTemp;
+      fileData = null;
+      respMsg = "";
+      if (data && data.result && data.result[0] && data.result[0].body) {
+        respData = data.result[0].body.innerText;
+        respPos = respData.indexOf("[{");
+        if (-1 !== respPos) {
+          respMsg = respData.substr(0, respPos);
+          respData = respData.substr(respPos);
+          fileData = $.parseJSON(respData);
+        }
+        else {
+          respMsg = respData;
+        }
+      }
+      else {
+        respData = data.result;
+        respPos = respData.indexOf("[{");
+        if (-1 !== respPos) {
+          respMsg = respData.substr(0, respPos);
+          respData = respData.substr(respPos);
+          fileData = eval(respData);
+        }
+        else {
+          respMsg = respData;
+        }
+      }
+      if (window.FormData !== undefined)
+      {
+        $("#id_img_loader_archivo" + iSeqRow).hide();
+      }
+      else
+      {
+        $("#id_ajax_loader_archivo" + iSeqRow).hide();
+      }
+      if (null == fileData) {
+        if ("" != respMsg) {
+          oTemp = {"htmOutput" : "<?php echo $this->Ini->Nm_lang['lang_errm_upld_admn']; ?>"};
+          scAjaxShowDebug(oTemp);
+        }
+        return;
+      }
+      if (fileData[0].error && "" != fileData[0].error) {
+        var uploadErrorMessage = "";
+        oResp = {};
+        if ("acceptFileTypes" == fileData[0].error) {
+          uploadErrorMessage = "<?php echo $this->form_encode_input($this->Ini->Nm_lang['lang_errm_file_invl']) ?>";
+        }
+        else if ("maxFileSize" == fileData[0].error) {
+          uploadErrorMessage = "<?php echo $this->form_encode_input($this->Ini->Nm_lang['lang_errm_file_size']) ?>";
+        }
+        else if ("minFileSize" == fileData[0].error) {
+          uploadErrorMessage = "<?php echo $this->form_encode_input($this->Ini->Nm_lang['lang_errm_file_size']) ?>";
+        }
+        else if ("emptyFile" == fileData[0].error) {
+          uploadErrorMessage = "<?php echo $this->form_encode_input($this->Ini->Nm_lang['lang_errm_file_empty']) ?>";
+        }
+        scAjaxShowErrorDisplay("table", uploadErrorMessage);
+        return;
+      }
+      $("#id_sc_field_archivo" + iSeqRow).val("");
+      $("#id_sc_field_archivo_ul_name" + iSeqRow).val(fileData[0].sc_ul_name);
+      $("#id_sc_field_archivo_ul_type" + iSeqRow).val(fileData[0].type);
+      $("#id_ajax_doc_archivo" + iSeqRow).html(fileData[0].name);
+      $("#id_ajax_doc_archivo" + iSeqRow).css("display", "");
+      checkDisplay = ("" == fileData[0].sc_random_prot.substr(12)) ? "none" : "";
+      $("#chk_ajax_img_archivo" + iSeqRow).css("display", checkDisplay);
+      $("#id_ajax_link_archivo" + iSeqRow).html(fileData[0].sc_random_prot.substr(12));
+    }
+  });
+
+} // scJQUploadAdd
+
+
+function scJQElementsAdd(iLine) {
+  scJQEventsAdd(iLine);
+  scEventControl_init(iLine);
+  scJQUploadAdd(iLine);
+} // scJQElementsAdd
+
